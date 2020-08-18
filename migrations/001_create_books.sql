a6bda004b58de2603a16d849de8b85b573b573f0
Mike Walker <michael.walker@softwire.com>
2020-08-18 18:24:34 +0100

    Add migration for books table.

---
+++ migrations/001_create_books.sql
@@ -0,0 +1,11 @@
+﻿DROP TABLE IF EXISTS book;
+
+CREATE TABLE book (
+    id                  SERIAL PRIMARY KEY,
+    title               VARCHAR(128) NOT NULL,
+    cover_image_url     VARCHAR(255),
+    published_date      DATE,
+    publisher           VARCHAR(255),
+    isbn                varchar(13),
+    author              VARCHAR(255) NOT NULL   
+);
